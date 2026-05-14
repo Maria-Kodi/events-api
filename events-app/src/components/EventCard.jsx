4ad79278520c874dd452770dd8803812027fbd7c
Maria Kodi <maria22newl@gmail.com>
2026-05-14 15:04:40 +0200

    setup: initialize events app structure

--- events-app/src/components/EventCard.jsx
+++ events-app/src/components/EventCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function App() {
+export default function EventCard() {
   return (
     <div>
       
     </div>
   )
 }
+
+
--- events-app/src/pages/CreateEvent.jsx
+++ events-app/src/pages/CreateEvent.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function App() {
+export default function CreateEvent() {
   return (
     <div>
       
     </div>
   )
 }
+

b373ec74ce5cff24c99e398dc5028ab8ec120df4
Maria Kodi <maria22newl@gmail.com>
2026-05-25 16:54:28 +0200

    fix: proper CORS configuration for production

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import './db.js';
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
 
+import './db.js';
 import router from './routes/index.js';
 import { setupSwagger } from './swagger.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
+
 const PORT = process.env.PORT ?? 3001;
 const isProduction = process.env.ENVIRONMENT === 'production';
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://events-api-1-su1i.onrender.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 if (!isProduction) {
@@ -23,10 +40,9 @@ app.use('/api', router);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log('\x1b[33m%s\x1b[0m', `Server running on \x1b[36m http://localhost:${PORT}`);
-  if (!isProduction)
-    console.log(
-      '\x1b[33m%s\x1b[0m',
-      `Swagger API docs available at \x1b[36m http://localhost:${PORT}/api-docs`
-    );
+  console.log(`Server running on http://localhost:${PORT}`);
+
+  if (!isProduction) {
+    console.log(`Swagger API docs: http://localhost:${PORT}/api-docs`);
+  }
 });
